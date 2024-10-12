26b34a6dcf3a7b5644d7133350e81695916c9573
adrian-brady <mail@adrianbrady.com>
2024-10-12 18:50:19 -0500

    enable expandtab

--- lua/adrianbrady/opts.lua
+++ lua/adrianbrady/opts.lua
@@ -15,6 +15,7 @@ vim.opt.number = true
 -- vim.opt.relativenumber = true
 
 vim.opt.mouse = "a" -- Enable mouse mode, can be useful for resizing splits for example!
+vim.opt.expandtab = true
 vim.opt.tabstop = 4
 vim.opt.relativenumber = true
 vim.opt.shiftwidth = 4
