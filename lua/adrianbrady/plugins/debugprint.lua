be38515118b082995f3d47bc02bef074a50df064
adrian-brady <mail@adrianbrady.com>
2024-12-07 12:34:52 -0600

    add debugprint plugin

---
+++ lua/adrianbrady/plugins/debugprint.lua
@@ -0,0 +1,32 @@
+return {
+	"andrewferrier/debugprint.nvim",
+	opts = {
+		keymaps = {
+			normal = {
+				plain_below = "g?p",
+				plain_above = "g?P",
+				variable_below = "g?v",
+				variable_above = "g?V",
+				variable_below_alwaysprompt = "",
+				variable_above_alwaysprompt = "",
+				textobj_below = "g?o",
+				textobj_above = "g?O",
+				toggle_comment_debug_prints = "",
+				delete_debug_prints = "",
+			},
+			insert = {
+				plain = "<C-G>p",
+				variable = "<C-G>v",
+			},
+			visual = {
+				variable_below = "g?v",
+				variable_above = "g?V",
+			},
+		},
+		commands = {
+			toggle_comment_debug_prints = "ToggleCommentDebugPrints",
+			delete_debug_prints = "DeleteDebugPrints",
+		},
+		-- … Other options
+	},
+}
