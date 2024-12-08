8cffbfaa50141bb49955b56e9bd85d90f31d5492
adrian-brady <mail@adrianbrady.com>
2024-12-08 10:42:38 -0600

    add diffview keymaps

--- lua/adrianbrady/plugins/diffview.lua
+++ lua/adrianbrady/plugins/diffview.lua
@@ -1,3 +1,18 @@
 return {
 	"sindrets/diffview.nvim",
+	keys = {
+		{ "<leader>do", "<cmd>DiffviewOpen<CR>", { desc = "[D]iffview[O]pen" } },
+		{ "<leader>dc", "<cmd>DiffviewClose<CR>", { desc = "[D]iffview[C]lose" } },
+		{ "<leader>dh", "<cmd>DiffviewClose<CR>", { desc = "[D]iffviewFile[H]istory" } },
+	},
+	opts = {
+		hooks = {
+			diff_buf_read = function()
+				-- Change local options in diff buffers
+				vim.opt_local.wrap = false
+				vim.opt_local.list = false
+				vim.opt_local.colorcolumn = { 80 }
+			end,
+		}, -- See |diffview-config-hooks|
+	},
 }
